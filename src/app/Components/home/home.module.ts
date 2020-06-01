625b27226f0e845815da2c8f8867b3f5e9150021
Sandeep Tiwari <sandeepatiwari12@gmail.com>
2020-06-01 13:22:27 +0530

    new design skaleton has been initiated

--- src/app/Components/home/home.module.ts
+++ src/app/Components/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { AngularMaterialModule } from 'src/app/Shared/_Modules/angular.material.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, AngularMaterialModule],
 })
 export class HomeModule {}
