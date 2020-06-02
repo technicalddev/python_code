8de88c3fa361d8829f53bdf170c3a10ddca7b67d
Sandeep Tiwari <sandeepatiwari12@gmail.com>
2020-06-02 16:47:22 +0530

    update: offering component has been created

--- src/app/Components/home/offering/offering.component.ts
+++ src/app/Components/home/offering/offering.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { offers } from './offering.mock';
 
 @Component({
   selector: 'mt-offering',
   templateUrl: './offering.component.html',
   styleUrls: ['./offering.component.scss'],
 })
-export class OfferingComponent implements OnInit {
-  constructor() {}
+export class OfferingComponent implements OnInit, OnDestroy {
+  mobileQuery: MediaQueryList;
+  mobileQueryListener: () => void;
+  baseURL: string = '../../../../assets/img/page/';
+  offers: any = [];
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher
+  ) {
+    this.mobileQuery = this.media.matchMedia('(max-width: 1110px)');
+    this.mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.offers = offers;
+  }
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this.mobileQueryListener);
+  }
 }
