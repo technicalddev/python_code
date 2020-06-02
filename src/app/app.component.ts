66fa132c7f9596bf56a32e0bdb06f0197965a75b
Sandeep Tiwari <sandeepatiwari12@gmail.com>
2020-06-02 12:25:06 +0530

    package.json update and some components also changes the selector name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mt-root-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'moneytor-in';
