484f08d616b5f01c18d7a5983fcd35ca40779878
Sandeep Tiwari <sandeepatiwari12@gmail.com>
2020-06-04 18:59:44 +0530

    clear dept component has been initiated without server side

--- src/app/Components/home/clear-dept/clear-dept.component.ts
+++ src/app/Components/home/clear-dept/clear-dept.component.ts
@@ -8,7 +8,23 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ClearDeptComponent implements OnInit {
   @Input() isMobile: boolean = false;
   @Input() baseURL: string = '';
+  postObj: any = {};
+  submitting: boolean;
   constructor() {}
 
   ngOnInit(): void {}
+
+  onChanges(model: any, value: any) {
+    this.postObj[model] = value;
+  }
+
+  onProcessContact() {
+    this.submitting = true;
+    const { name, email, phone } = this.postObj;
+    if (!name || !email || !phone) {
+      this.submitting = false;
+      return;
+    }
+    console.log('final postObj', this.postObj);
+  }
 }
