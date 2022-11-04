64b7d09d2c4410a9cc424e72edf1f1beffd91ae7
darshana <techsupport@moneytor.in>
2022-11-04 14:37:58 +0530

    testimonial added

--- src/app/Components/home/testimonials/testimonials.mock.ts
+++ src/app/Components/home/testimonials/testimonials.mock.ts
@@ -1,7 +1,7 @@
 export const clientTestimonials = [
   {
     statement:
-      '“I can surely say Moneytor is the best place and has the best people who take proper measures and help people in closing their account without any harassment like other companies..”',
+      '“I can surely say Moneytor is the best place and has the best people who take proper measures and help people in closing their account without any harassment like other companies.Jitendra Rai in my case was calm and helped me so much in getting out of my debt, by helping in easy emis and regular follow up. He is a good man and the other recovery team should learn from him and moneytor.”',
     clientImg: '../../../../assets/img/clientImage.jpg',
     clientName: 'Ishan Vandoor, ',
     clientCity: 'Mumbai',
@@ -19,5 +19,12 @@ export const clientTestimonials = [
     clientImg: '../../../../assets/img/clientImage.jpg',
     clientName: 'Suraj Gupta',
     clientCity: ',Mumbai',
+  },
+  {
+    statement:
+      '“Thank you for your response. And Also Thank You For MR Dibakar Paul For Very Help Fully support.”',
+    clientImg: '../../../../assets/img/clientImage1.jpg',
+    clientName: 'Hardikbhai Beladiya, ',
+    clientCity: 'Mumbai',
   },
 ];
