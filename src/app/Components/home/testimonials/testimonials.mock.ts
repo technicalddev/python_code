6b682e50ca50d7f7c9cae17e7b0e9f3d086f7264
darshana <techsupport@moneytor.in>
2023-02-06 12:17:02 +0530

    testimonial added

--- src/app/Components/home/testimonials/testimonials.mock.ts
+++ src/app/Components/home/testimonials/testimonials.mock.ts
@@ -1,6 +1,13 @@
 export const clientTestimonials = [
   {
     statement:
+      '“I am honoured to have be proud of the fact that you have the power to rise above any situation and deliver the best results no matter the circumstances. Excellent work”',
+    clientImg: '../../../../assets/img/clientImage1.jpg',
+    clientName: 'Arbeen Bhatra,',
+    clientCity: 'Nabarangpur,Odisha',
+  },
+  {
+    statement:
       '“I can surely say Moneytor is the best place and has the best people who take proper measures and help people in closing their account without any harassment like other companies.Jitendra Rai in my case was calm and helped me so much in getting out of my debt, by helping in easy emis and regular follow up. He is a good man and the other recovery team should learn from him and moneytor.”',
     clientImg: '../../../../assets/img/clientImage.jpg',
     clientName: 'Ishan Vandoor, ',
