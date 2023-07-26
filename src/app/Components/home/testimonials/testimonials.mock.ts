10d7218508d01088f098c24498be78f347ae5877
Karthik Kalyanapu <techsupport@moneytor.in>
2023-07-26 15:43:28 +0530

    testimonals icon changes

--- src/app/Components/home/testimonials/testimonials.mock.ts
+++ src/app/Components/home/testimonials/testimonials.mock.ts
@@ -9,28 +9,28 @@ export const clientTestimonials = [
   {
     statement:
       '“I can surely say Moneytor is the best place and has the best people who take proper measures and help people in closing their account without any harassment like other companies.Jitendra Rai in my case was calm and helped me so much in getting out of my debt, by helping in easy emis and regular follow up. He is a good man and the other recovery team should learn from him and moneytor.”',
-    clientImg: '../../../../assets/img/clientImage.jpg',
+    clientImg: '../../../../assets/img/client2.png',
     clientName: 'Ishan Vandoor, ',
     clientCity: 'Mumbai',
   },
   {
     statement:
       '“Thanks for your Kind support. I really appreciate Mrs. Porismita. She was very Positive and Helping nature.”',
-    clientImg: '../../../../assets/img/clientImage1.jpg',
+    clientImg: '../../../../assets/img/client3.png',
     clientName: 'Raghunath Giri, ',
     clientCity: 'Mumbai',
   },
   {
     statement:
       '“Best way to be debt free. Moneytor helped me get rid of loan in the easiest way possible.”',
-    clientImg: '../../../../assets/img/clientImage.jpg',
+    clientImg: '../../../../assets/img/client4.png',
     clientName: 'Suraj Gupta',
     clientCity: ',Mumbai',
   },
   {
     statement:
       '“Thank you for your response. And Also Thank You For MR Dibakar Paul For Very Help Fully support.”',
-    clientImg: '../../../../assets/img/clientImage1.jpg',
+    clientImg: '../../../../assets/img/client5.png',
     clientName: 'Hardikbhai Beladiya, ',
     clientCity: 'Mumbai',
   },
