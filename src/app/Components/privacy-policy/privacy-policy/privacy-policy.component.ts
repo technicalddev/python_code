c9232281c612cd65521d707aabd1d2eea31c54d2
Karthik Kalyanapu <techsupport@moneytor.in>
2023-09-15 16:29:23 +0530

    adde privacy policy

---
+++ src/app/Components/privacy-policy/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-privacy-policy',
+  templateUrl: './privacy-policy.component.html',
+  styleUrls: ['./privacy-policy.component.scss'],
+})
+export class PrivacyPolicyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
