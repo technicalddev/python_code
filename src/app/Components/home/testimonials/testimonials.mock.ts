879a5148653c374f63aaa8acf1a74c46fad34018
Karthik Kalyanapu <techsupport@moneytor.in>
2023-12-22 11:12:46 +0530

    added new testimonials

--- src/app/Components/home/testimonials/testimonials.mock.ts
+++ src/app/Components/home/testimonials/testimonials.mock.ts
@@ -1,6 +1,13 @@
 export const clientTestimonials = [
   {
     statement:
+      "“I am happy to share my feedback for Tejaswi. One of my mobikwik load was stuck from the last 18 months. I told her the whole scenario and she calmly heard and fully cooperated me. Because of her I'm able to settle my loan amount.I'm very satisfied with her work and really appreciate her. She is polite, gentle and her customer support services are excellent.Thanks to her again for the wonderful work.“",
+    clientImg: '../../../../assets/img/client2.png',
+    clientName: 'Junaid Alam,',
+    clientCity: 'Bihar',
+  },
+  {
+    statement:
       '“The Complete interaction to repay the debt, which has done by your executive by name Riya it was very good.”',
     clientImg: '../../../../assets/img/client4.png',
     clientName: 'Shivaprasad Patne, ',
