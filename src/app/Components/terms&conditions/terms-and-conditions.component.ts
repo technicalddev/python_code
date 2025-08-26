e4bb5d3b7ce933ea8e159d84b841dca9b893c99d
technicalddev <dev@moneytor.in>
2025-08-26 12:51:04 +0530

    blank

--- src/app/Components/terms&conditions/terms-and-conditions.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'mt-terms-and-conditions',
-  templateUrl: './terms-and-conditions.component.html',
-  styleUrls: ['./terms-and-conditions.component.scss'],
-})
-export class TermsAndConditionsComponent {}
--- src/app/shared/_Models/pagination.ts
+++
@@ -1,2 +0,0 @@
-export const pagination = [10, 20, 50, 100, 200, 500];
-export const rowsPerPage = 20;
